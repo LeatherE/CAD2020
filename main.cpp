0cf0ed39275f1581b73483dcc75094240dbb68b5
李孟憬 <amy26656@gmail.com>
2020-06-03 03:35:23 +0800

    build encoder

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ using namespace std;
 int main(){
 	int i;
 	Circuit c;
+
+	vector<string> w_tmp;
+	vector<Gate> g_tmp;
 	
 	c.ReadInList();
 	c.ReadOutList();
@@ -19,11 +22,32 @@ int main(){
 	c.BuildAdjList();
 	//c.PrintCircuit();
 	c.topsort_Call();
-	cout<<"number of xgate: "<<c.num_of_xgate<<endl;
-	//for(i = 0; i < c
+	//cout<<"number of xgate: "<<c.num_of_xgate<<endl;
+
 	
+
 	
+	/*w_tmp = c.GetInList();
+	cout<<"input size: "<<w_tmp.size()<<endl;
+	cout<<"input: "<<endl;
+	for(i = 0; i < w_tmp.size(); i++)
+		cout<<w_tmp[i]<<" ";
+	cout<<endl;
 	
+	w_tmp = c.GetOutList();
+	cout<<"output: "<<endl;
+	for(i = 0; i < w_tmp.size(); i++)
+		cout<<w_tmp[i]<<" ";
+	cout<<endl;
+	
+	cout<<"wire: "<<endl;
+	for(i = 0; i < w_tmp.size(); i++)
+		cout<<w_tmp[i]<<" ";
+	cout<<endl;*/
+	Encoder e(c.GetInList(), c.GetOutList(), c.GetGateList(), c.first_xgate, c.top_order);
+
+	e.EncodeCircuit();
+	e.PrintResult();
 	return 0;
 }
 
