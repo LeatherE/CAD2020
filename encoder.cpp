8fe0bcf1c1304affc99da16e64f3c63b86ae7754
timlee-tw <yitingli.yt@gmail.com>
2020-06-10 20:30:54 +0800

    test for git commit

--- encoder.cpp
+++ encoder.cpp
@@ -6,6 +6,8 @@ using namespace std;
 #include <sstream>
 #include <algorithm>
 
+//test
+
 Encoder::Encoder(vector<string> Tinput, vector<string> Toutput, vector<Gate> Tgate, int first_xgate, int *T_top_order){
 	TargetIn = Tinput;
 	TargetOut = Toutput;
